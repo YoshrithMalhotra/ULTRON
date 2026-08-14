53008c0a1085ad35497454d0936e4e7f7d51091a
YoshrithMalhotra <malhotrayoshrith@gmail.com>
2026-08-14 23:30:26 +0530

    running server

--- cmd/ultron/main.go
+++ cmd/ultron/main.go
@@ -1,8 +1,19 @@
 package main
 
-import(
-	"fmt"
+import (
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Ultron is online",
+		})
+	})
+
+	router.Run(":8080")
+}
